dfbcd3069a8242201572a990ccdbb0232dabb666
Filip Rudek <rudek.filip@gmail.com>
2022-05-10 15:40:17 +0200

    Connect to dataBase

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import Cors from 'cors'
 import Helmet from 'helmet'
 import RateLimit from 'express-rate-limit'
 import Morgan from 'morgan'
+import Mongoose from 'mongoose'
 
 const app = Express()
 
 const port = process.env.PORT
+const dbPassword = process.env.DB_PASSWORD
+const dbName = process.env.DB_NAME
+
 const limiter = RateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
@@ -20,5 +24,11 @@ app.use(Helmet())
 app.use(limiter)
 app.use(Morgan('tiny'))
 app.use(Express.json())
+
+Mongoose.connect(
+  `mongodb+srv://admin:${dbPassword}@cluster0.dclwf.mongodb.net/${dbName}?retryWrites=true&w=majority`
+)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log(`Could not connect to MongoDB... ${err}`))
 
 app.listen(port, () => console.log(`listening on port ${port}`))
